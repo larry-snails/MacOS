7a02ce64b067bc0c9674bd773584d200ba70eb68
吕志滨 <lvzhibin@lvzhibindeMacBook-Pro.local>
2019-07-25 08:53:52 +0800

    first commit

---
+++ macos_helloworld/macos_helloworld/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  macos_helloworld
+//
+//  Created by 吕志滨 on 2019/7/25.
+//  Copyright © 2019 吕志滨. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
